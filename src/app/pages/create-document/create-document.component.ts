bef1994d8ff24434d162be27b4bfd7220a689c8a
PABON ESTRELLA Oscar Andres <oscarandres.pabonestrella@ext.europarl.europa.eu>
2024-11-21 13:24:23 +0100

    create document upload files sections

--- src/app/pages/create-document/create-document.component.ts
+++ src/app/pages/create-document/create-document.component.ts
@@ -1,40 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import data from "./data.json";
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms';
+} from "@angular/forms";
+import { BadgeComponent } from "../../components/badge/badge.component";
 @Component({
-  selector: 'app-create-document',
+  selector: "app-create-document",
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
-  templateUrl: './create-document.component.html',
-  styleUrl: './create-document.component.css',
+  imports: [ReactiveFormsModule, CommonModule, BadgeComponent],
+  templateUrl: "./create-document.component.html",
+  styleUrl: "./create-document.component.css",
 })
 export class CreateDocumentComponent implements OnInit {
   documentForm: FormGroup;
-  formats = ['Digital', 'Paper'];
-  languages = ['English', 'Spanish', 'French', 'German'];
+  documentTypes = data.documentTypes;
+  showDisplayTitleInput = false;
+  showFiles = false;
+  formats = ["Digital", "Paper"];
+  languages = ["English", "Spanish", "French", "German"];
+  formatControl: FormControl = new FormControl(false);
 
   constructor(private fb: FormBuilder) {
     this.documentForm = this.fb.group({
-      documentTitle: ['', Validators.required],
-      documentType: ['Letter', Validators.required],
-      documentDate: ['', Validators.required],
-      format: ['Digital'],
-      mainLanguage: ['English'],
-      marking: ['Financial Matters'],
+      documentTitle: ["", Validators.required],
+      documentType: ["Letter", Validators.required],
+      documentDate: ["", Validators.required],
+      format: ["Digital"],
+      mainLanguage: ["English"],
+      marking: ["Financial Matters"],
       draftingMode: [false],
-      embargoUntil: [''],
-      from: ['', Validators.required],
-      to: ['', Validators.required],
+      embargoUntil: [""],
+      from: ["", Validators.required],
+      to: ["", Validators.required],
       isAnonymous: [false],
       annexesScanned: [true],
       translationRequested: [false],
     });
   }
+
+  files = [
+    {
+      name: "SCAN_Menu_preview.pdf",
+      icon: "bi-file-earmark-pdf",
+      type: "Main element",
+      version: 0.1,
+      date: "11/11/2023",
+    },
+    {
+      name: "User_Manual_v1.docx",
+      icon: "bi-file-earmark-word",
+      type: "Translation",
+      version: 0.9,
+      date: "10/05/2023",
+    },
+    {
+      name: "Financial_Report_Q3.xlsx",
+      icon: "bi-file-earmark-excel",
+      type: "Translation",
+      version: 3.2,
+      date: "09/30/2023",
+    },
+  ];
 
   ngOnInit(): void {}
 
@@ -49,6 +80,12 @@ export class CreateDocumentComponent implements OnInit {
   }
 
   onSaveTemporarily(): void {
-    console.log('Saving temporarily:', this.documentForm.value);
+    console.log("Saving temporarily:", this.documentForm.value);
+  }
+  toggleDisplayTitle() {
+    this.showDisplayTitleInput = !this.showDisplayTitleInput;
+  }
+  toggleFiles() {
+    this.showFiles = !this.showFiles;
   }
 }
